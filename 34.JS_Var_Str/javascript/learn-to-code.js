d56f7846a20e4eeb67e58f155d646822e807a9c4
Anthony Chen <anchen082019@gmail.com>
2021-08-21 22:27:08 -0400

    new cahnges

---
+++ 34.JS_Var_Str/javascript/learn-to-code.js
@@ -0,0 +1,7 @@
+var name = "Tony";
+var age = 18;
+var message = "Hi, My name is " + name + " and I am " + age + " years old!";
+                        
+//alert(message);
+            
+console.log(message);

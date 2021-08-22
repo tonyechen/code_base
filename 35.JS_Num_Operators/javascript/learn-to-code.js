4aca2732e62e0c0e180fa0092f2938eb33e49520
Anthony Chen <anchen082019@gmail.com>
2021-08-21 23:04:10 -0400

    new changes

---
+++ 35.JS_Num_Operators/javascript/learn-to-code.js
@@ -0,0 +1,8 @@
+var myAccountBalance = 300;
+var nikeSBShoes = 799.23;
+
+if (nikeSBShoes <= myAccountBalance) {
+    myAccountBalance -= nikeSBShoes;
+    console.log("We just bought some dope shoes!");
+    console.log("Account Balance: " + myAccountBalance);
+}

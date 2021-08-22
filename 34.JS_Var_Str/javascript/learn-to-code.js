3ba5857e4910dd653bdeb5fded6598251e9e97a0
Anthony Chen <anchen082019@gmail.com>
2021-08-21 22:42:54 -0400

    new changes

--- 34.JS_Var_Str/javascript/learn-to-code.js
+++ 34.JS_Var_Str/javascript/learn-to-code.js
@@ -1,7 +1,15 @@
-var name = "Tony";
-var age = 18;
-var message = "Hi, My name is " + name + " and I am " + age + " years old!";
-                        
-//alert(message);
-            
-console.log(message);
+// var name = "Tony";
+// var age = 18;
+// var shootingScore = 45.6;
+
+// var message = "Hi, My name is " + name + " and I am " + age + " years old!";
+
+var firstName = "John";
+var lastName = "JacobJingleHeimerSmith";
+var dateOfBirth = "10-09-82";
+var age = 23;
+var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+
+var loginWelcomeMessage = "Welcome, " + firstName + ". Happy "+ age + "rd birthday!";
+
+console.log(loginWelcomeMessage);

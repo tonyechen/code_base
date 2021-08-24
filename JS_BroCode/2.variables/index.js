8259c12d43b9470cad8fdf4df5a4565478cd2987
Anthony Chen <anchen082019@gmail.com>
2021-08-23 20:53:11 -0400

    new changes

---
+++ JS_BroCode/2.variables/index.js
@@ -0,0 +1,13 @@
+var age = 18;
+var firstName = "Tony";
+var lastName = "Chen";
+var online = true;
+var whatever = null;
+
+age = age + 1;
+var fullName = firstName + " " + lastName;
+
+console.log("Your age is ", age);
+console.log("Hello", fullName);
+console.log("Are you online?: ", online);
+console.log(whatever);
---
+++ JS_BroCode/3.arithmetic_expressions/index.js
@@ -0,0 +1,22 @@
+/*
+    arithemitc expression is some combination of opreand (values, variables, etc.) operators (+ - * /) that can be evaluted to a vlue
+
+*/
+
+var friends = 10;
+
+// friends = friends + 1;
+// friends = friends - 1;
+// friends = friends * 2;
+// friends = friends / 2;
+// friends = friends **2;
+// var remainingFriends = friends % 3;
+
+// friends += 1;
+// friends -= 1;
+// friends *= 2;
+// friends /= 2;
+friends %= 3;
+
+friends **= 2;
+console.log(friends);

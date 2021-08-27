519bf7fbcbc27b88ea2017deae616d335d072fff
Anthony Chen <anchen082019@gmail.com>
2021-08-26 21:25:49 -0400

    new changes

---
+++ JS_BroCode/9.if_statements/index.js
@@ -0,0 +1,23 @@
+var age = 18;
+
+if(age >= 65) {
+    console.log("You are a senior!");
+} 
+else if(age >= 18) {
+    console.log("You are an adult");
+}
+else if(age < 0){
+    console.log("You haven't been born yet!")
+}
+else {
+    console.log("You are a child!");
+}
+
+var online = true;
+
+if(online) {
+    console.log("Bro is online");
+}
+else {
+    console.log("Bro is offline");
+}

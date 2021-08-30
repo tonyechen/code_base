b3fe8b30f6e345897b9a1c4310526d13ddbd985c
Anthony Chen <anchen082019@gmail.com>
2021-08-30 15:30:35 -0400

    new changes

---
+++ JS_BroCode/18.Date_object/index.js
@@ -0,0 +1,34 @@
+// date object = represents a moment in time since epoch (reference point)
+
+date = new Date(0);
+date = new Date();
+date = new Date(2022, 4, 30, 10, 10, 0); // year month day hour minutes seconds miliseconds
+date = new Date("April 30, 2022 16:20:01:02");
+
+let year = date.getFullYear();
+let month = date.getMonth();
+let dayOfWeek = date.getDay();
+let dayofMonth = date.getDate();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let milliSeconds = date.getMilliseconds();
+
+console.log(year);
+console.log(month);
+console.log(dayofMonth);
+console.log(dayOfWeek);
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
+console.log(milliSeconds);
+
+date.setFullYear(2023);
+date.setMonth(11);
+date.setDate(25);
+date.setHours(0);
+date.setMinutes(0);
+date.setSeconds(0);
+date.setMilliseconds(0);
+
+console.log(date);

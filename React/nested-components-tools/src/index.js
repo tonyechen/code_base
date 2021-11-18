2f9933e3a8f784a31bb2b04ebecc33b77653ed09
Anthony Chen <anchen082019@gmail.com>
2021-11-17 19:37:26 -0800

    new changes

---
+++ React/nested-components-tools/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// Nested Components, React Tools
+
+function Greeting() {
+  return (
+    <div>
+      <div>
+        <Person />
+        <Message />
+      </div>
+    </div>
+  );
+}
+
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+};
+ReactDom.render(<Greeting />, document.getElementById("root"));

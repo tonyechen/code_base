7e44905cad8ebf3ee4321fa99dabc0092ce2cc8a
Anthony Chen <anchen082019@gmail.com>
2021-11-18 00:37:11 -0800

    new changes

--- React/nested-components-tools/src/index.js
+++ React/nested-components-tools/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// CSS
+import "./index.css";
+
 // Nested Components, React Tools
 
 // function Greeting() {
@@ -22,7 +25,9 @@ import ReactDom from "react-dom";
 
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
+      <Book />
+      <Book />
       <Book />
       <Book />
       <Book />
@@ -33,8 +38,46 @@ function BookList() {
   );
 }
 
+const author = "Tony chen";
 const Book = () => {
-  return <article>this is a car</article>;
+  const title = "I Love Ramen to death";
+  return (
+    <article className="book">
+      <img src="https://www.elmundoeats.com/wp-content/uploads/2021/02/FP-Quick-30-minutes-chicken-ramen.jpg" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {/* must be an expression that returns a value*/}
+    </article>
+  );
 };
+
+// const Book = () => {
+//   return (
+//     <article className="book">
+//       <Image />
+//       <Title />
+//       <Author />
+//     </article>
+//   );
+// };
+
+// const Image = () => {
+//   return (
+//     <img
+//       src="https://www.elmundoeats.com/wp-content/uploads/2021/02/FP-Quick-30-minutes-chicken-ramen.jpg"
+//       width="300px"
+//       height="300px"
+//     ></img>
+//   );
+// };
+
+// const Title = () => <h1>I would die for Ramen</h1>;
+
+// // JSX + CSS
+// const Author = () => (
+//   <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: ".25rem" }}>
+//     by tony
+//   </h4>
+// );
 
 ReactDom.render(<BookList />, document.getElementById("root"));

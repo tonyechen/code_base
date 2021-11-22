64a0f49b24790562ffffa462166eadcc1b199ea3
Anthony Chen <anchen082019@gmail.com>
2021-11-22 00:49:04 -0800

    new changes

--- React/events-basics/src/index.js
+++ React/events-basics/src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
+import "./index.css";
+const books = [
+  {
+    id: 1,
+    author: "Tony chen",
+    title: "I Love Ramen to death",
+    img: "https://www.elmundoeats.com/wp-content/uploads/2021/02/FP-Quick-30-minutes-chicken-ramen.jpg",
+  },
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  {
+    id: 2,
+    author: "Audrey Chen",
+    title: "I Love Corgis to death",
+    img: "https://iheartdogs.com/wp-content/uploads/2021/02/Cute_Corgi_face-e1613588692370.jpg",
+  },
+
+  {
+    id: 3,
+    author: "John Doe",
+    title: "Some Title",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+];
+
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
+
+  const { img, title, author, children } = props;
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1
+        onClick={() => {
+          console.log(title);
+        }}
+      >
+        {title}
+      </h1>
+      <h4>{author}</h4>
+      <button type="submit" onClick={clickHandler}>
+        reference example
+      </button>
+      {children}
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
